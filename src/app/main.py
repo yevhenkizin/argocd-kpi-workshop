9786265e72875da7555b4fd3ab26c4128fc7e2fd
Yevhen Kizin <yevhen@4admin.space>
2022-12-13 13:07:56 +0200

    update version

--- src/app/main.py
+++ src/app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, World! This is v2!'
+    return 'Hello, World! This is v3 with error!'
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=80)
